89b9ca9dc1eb8dd299389b5fdfeb7f1598d68c42
ABIDULLAH786 <abidullah.se@gmail.com>
2022-03-26 11:59:03 -0700

    Validation of Form is added

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button} from "reactstrap";
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input, Col, Button, FormFeedback} from "reactstrap";
 
 class Contact extends React.Component {
      
@@ -13,11 +13,17 @@ class Contact extends React.Component {
             agree: false,
             email: "",
             message: "",
-            contactType:""
-            
+            contactType:"",
+            touched:{
+                fName: false,
+                lName: false,
+                phone: false,
+                email: false
+            }
         }
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleBlur = this.handleBlur.bind(this)
     }
     handleChange(event){
         const {name, value, type, checked} = event.target
@@ -26,13 +32,40 @@ class Contact extends React.Component {
             :
             this.setState({ [name]: value }) 
     }
-    
+    handleBlur = (field) => (e)=>{
+        this.setState({
+            touched: { ...this.state.touched, [field]:true}
+        })
+    } 
+    validate(fName,lName, phone,email){
+        const errors = {
+            fName: "",
+            lName: "",
+            phone: "",
+            email: ""
+        }
+        if(this.state.touched.fName && (fName.length < 3 || fName.length >10))
+            errors.fName = "First Name Should be between 3-10";
+        if(this.state.touched.lName && (lName.length < 3 || lName.length >10))
+            errors.lName = "Last Name Should be between 3-10";
+
+        const reg = /^\d+$/;
+        if(this.state.phone && !reg.test(phone))
+            errors.phone = "Phone number should contain only number";
+        
+        const emailFormate = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        if(this.state.email && !emailFormate.test(email))
+            errors.email = "Invalid email"
+
+            return errors;
+    }
     handleSubmit(e){
         alert(JSON.stringify("Done"))
         alert(JSON.stringify(this.state))
         e.preventDefault();
     }
     render(){
+        const errors = this.validate(this.state.fName, this.state.lName, this.state.phone, this.state.email)
     return (
         <div className="container">
             <div className="row">
@@ -56,10 +89,14 @@ class Contact extends React.Component {
                 <FormGroup row >
                     <Label htmlFor="fName" md={2} sm={3}>First Name</Label>
                     <Col className="col-10 col-md-5">
-                        <Input  type="text" name="fName" placeholder="First Name" id="fName"
+                        <Input type="text" name="fName" placeholder="First Name" id="fName"
                             value={this.state.fName}
                             onChange={this.handleChange}
+                            onBlur={this.handleBlur("fName")}
+                            valid={this.state.touched.fName && this.state.fName !=="" && errors.fName === ''}
+                            invalid={this.state.lName !== "" && errors.fName !== ''}
                         />
+                        <FormFeedback>{errors.fName}</FormFeedback>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
@@ -68,7 +105,12 @@ class Contact extends React.Component {
                         <Input  type="text" name="lName" placeholder="Last Name" id="lName"
                             value={this.state.lName}
                             onChange={this.handleChange}
+                            onBlur={this.handleBlur("lName")}
+                            valid={this.state.touched.lName && this.state.lName !== "" && errors.lName === ''}
+                            invalid={this.state.lName !== "" && errors.lName !== ''}
                         />
+                        <FormFeedback>{errors.lName}</FormFeedback>
+
                     </Col>
                 </FormGroup>
                 <FormGroup row >
@@ -77,7 +119,12 @@ class Contact extends React.Component {
                         <Input  type="text" name="phone" placeholder="Phome" id="phone"
                             value={this.state.phone}
                             onChange={this.handleChange}
+                            onBlur={this.handleBlur("phone")}
+                            valid={this.state.touched.phone && this.state.phone !=="" && errors.phone === ''}
+                            invalid={errors.phone !== ''}
                         />
+                        <FormFeedback>{errors.phone}</FormFeedback>
+
                     </Col>
                 </FormGroup>
                 <FormGroup row >
@@ -86,11 +133,16 @@ class Contact extends React.Component {
                         <Input  type="email" name="email" placeholder="Email" id="email"
                             value={this.state.email}
                             onChange={this.handleChange}
+                            onBlur={this.handleBlur("email")}
+                            valid={this.state.touched.email && this.state.email !=="" && errors.email === ''}
+                            invalid={errors.email !== ''}
                         />
+                        <FormFeedback>{errors.email}</FormFeedback>
+
                     </Col>
                 </FormGroup>
                 <FormGroup row >
-                    <Col className="col-7 col-md-3 offset-md-2">
+                    <Col className="col-12 col-md-4 offset-md-2">
                         <FormGroup check>
                             <Label check>
                                 <Input  type="checkbox" name="agree" 
@@ -102,7 +154,7 @@ class Contact extends React.Component {
                             </Label>
                         </FormGroup>    
                     </Col>
-                    <Col className="col-5  col-md-2">
+                    <Col className="col-12  col-md-3">
                     <Label check>
                                 <Input  type="select" name="contactType" 
                                     onChange={this.handleChange}
